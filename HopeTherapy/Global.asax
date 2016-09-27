2b4a319b31213942b423572e746d696cc297fde8
Duncan Hayward <Duncan Hayward>
2016-09-27 18:51:30 -0400

    add HopeTherapy

---
+++ HopeTherapy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HopeTherapy.MvcApplication" Language="C#" %>
---
+++ HopeTherapy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HopeTherapy.Startup))]
+namespace HopeTherapy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
