337e892dd41be840acd22d2c45d0ae87bf540ef3
georgesaltz <george.saltzgiver@gmail.com>
2016-10-10 19:56:05 -0400

    create utilities class

---
+++ HopeTherapy/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using HopeTherapy.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace HopeTherapy
+{
+    public static class Utilities
+    {
+        public static readonly SqlDataAccess Sql = new SqlDataAccess(ConfigurationManager.ConnectionStrings["HopeTherapyIMS"].ConnectionString);
+    }
+
+
+}
