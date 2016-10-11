8e9f0c97670bb6f5960b4e45382c81d11908449f
georgesaltz <george.saltzgiver@gmail.com>
2016-10-10 20:21:01 -0400

    demo

---
+++ HopeTherapy/Controllers/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using HopeTherapy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HopeTherapy.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            var model = new Login { Username = "test", Password = "password" };
+            return View(model);
+        }
+    }
+}
