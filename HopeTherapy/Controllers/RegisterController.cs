e44fe554ee11465d53c8af9eb487bc08a11ad1f1
georgesaltz <george.saltzgiver@gmail.com>
2016-10-11 09:05:56 -0400

    Added controller, view, and model for Register

---
+++ HopeTherapy/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using HopeTherapy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HopeTherapy.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Register model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Todo: Model is not valid.
+                return View(model);
+            }
+
+            // Todo: Check database
+            return View(model);
+        }
+    }
+}
---
+++ HopeTherapy/Models/Register.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HopeTherapy.Models
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "Username is required"), MaxLength(50, ErrorMessage = "Username must be less than 50 chars"), DisplayName("Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required"), MaxLength(50, ErrorMessage = "Password must be less than 50 chars"), DisplayName("Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Email is required"), DisplayName("Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required"), DisplayName("FirstName")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required"), DisplayName("LastName")]
+        public string LastName { get; set; }
+
+
+    }
+}
