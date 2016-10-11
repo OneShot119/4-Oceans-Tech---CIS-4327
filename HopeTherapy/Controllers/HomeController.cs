3c77e1c889489126fef6c6ab53ed59942fdd0e06
georgesaltz <george.saltzgiver@gmail.com>
2016-10-10 20:07:50 -0400

    add about page

--- HopeTherapy/Controllers/HomeController.cs
+++ HopeTherapy/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace HopeTherapy.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
