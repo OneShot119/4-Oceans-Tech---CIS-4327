251ccac812ca240b20590eae6992d5917a11efeb
georgesaltz <george.saltzgiver@gmail.com>
2016-10-13 17:27:07 -0400

    Partial integration with database for registration

--- HopeTherapy/Controllers/RegisterController.cs
+++ HopeTherapy/Controllers/RegisterController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using HopeTherapy.DataAccess;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace HopeTherapy.Controllers
 {
@@ -27,7 +30,16 @@ namespace HopeTherapy.Controllers
             }
 
             // Todo: Check database
-            var user = new User();
+            //string sql = "INSERT INTO Register VALUES("+model.Username+ ", " + model.Password + ")";
+            //int temp = DataAccess.SqlDataAccess.ExecuteCommand(sql);
+            using (var cn = new SqlConnection(ConfigurationManager.ConnectionStrings["HopeTherapyIMS"].ConnectionString))
+            {
+                string sql = "INSERT INTO [UserName, Password, Email, FirstName, LastName] FROM [dbo].[Register] VALUES[" + model.Username + ", " + model.Password + ", " + model.Email + ", " + model.FirstName + ", " + model.LastName + "]";
+                //Todo: Not working
+                //int temp = SqlDataAccess.ExecuteCommand(sql, null);
+            }
+
+                var user = new User();
             user.UserName = model.Username;
             user.Password = model.Password;
             FormsAuthentication.SetAuthCookie(user.UserName, true);
