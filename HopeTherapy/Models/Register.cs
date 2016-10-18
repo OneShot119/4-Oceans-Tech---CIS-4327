70bbef2ef53426c435a65acea2c7f19e819bd648
georgesaltz <george.saltzgiver@gmail.com>
2016-10-17 21:01:14 -0400

    register progress

--- HopeTherapy/Models/Register.cs
+++ HopeTherapy/Models/Register.cs
@@ -12,19 +12,19 @@ namespace HopeTherapy.Models
         [Required(ErrorMessage = "Username is required"), MaxLength(50, ErrorMessage = "Username must be less than 50 chars"), DisplayName("Username")]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required"), MaxLength(50, ErrorMessage = "Password must be less than 50 chars"), DisplayName("Password")]
+        [Required(ErrorMessage = "Password is required"), MaxLength(50, ErrorMessage = "Password must be less than 50 chars"), DisplayName("Password"), DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Email is required"), DisplayName("Email")]
+        [Required(ErrorMessage = "Email is required"), EmailAddress(ErrorMessage = "Invalid email address."), DisplayName("Email"), DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "FirstName is required"), DisplayName("FirstName")]
+        [Required(ErrorMessage = "FirstName is required"), DisplayName("First Name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "LastName is required"), DisplayName("LastName")]
+        [Required(ErrorMessage = "LastName is required"), DisplayName("Last Name")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "AccessKey is required"), DisplayName("AccessKey")]
+        [Required(ErrorMessage = "AccessKey is required"), DisplayName("Access Key")]
         public string AccessKey { get; set; }
 
     }
