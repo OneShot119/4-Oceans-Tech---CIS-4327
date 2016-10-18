2a22c89df8ae7e22a3150692f2f3ce211fa214fe
Britainy Richardson <Britainy Richardson>
2016-10-18 16:31:30 -0400

    Donor

---
+++ HopeTherapy/Controllers/Donor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HopeTherapy.Models;
+using System.Web.Mvc;
+
+namespace HopeTherapy.Controllers
+{
+    public class DonorController : Controller
+    {
+        // GET: Donor
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Donor model)
+        {
+
+            // Todo: Check database
+            return View(model);
+        }
+    }
+}
