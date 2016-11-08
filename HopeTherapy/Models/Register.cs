53e1bb9650065921a6a383867abeb3fc507ef5c9
georgesaltz <george.saltzgiver@gmail.com>
2016-11-07 19:18:03 -0500

    add max length to register

--- HopeTherapy/Models/Register.cs
+++ HopeTherapy/Models/Register.cs
@@ -15,13 +15,13 @@ namespace HopeTherapy.Models
         [Required(ErrorMessage = "Password is required"), MaxLength(50, ErrorMessage = "Password must be less than 50 chars"), DisplayName("Password"), DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Email is required"), EmailAddress(ErrorMessage = "Invalid email address."), DisplayName("Email"), DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Email is required"), MaxLength(50, ErrorMessage = "Email must be less than 50 chars"), EmailAddress(ErrorMessage = "Invalid email address."), DisplayName("Email"), DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "FirstName is required"), DisplayName("First Name")]
+        [Required(ErrorMessage = "FirstName is required"), MaxLength(50, ErrorMessage = "First Name must be less than 50 chars"), DisplayName("First Name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "LastName is required"), DisplayName("Last Name")]
+        [Required(ErrorMessage = "LastName is required"), MaxLength(50, ErrorMessage = "Last Name must be less than 50 chars"), DisplayName("Last Name")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "AccessKey is required"), DisplayName("Access Key")]
