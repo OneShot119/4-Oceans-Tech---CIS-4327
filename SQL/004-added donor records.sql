dce91f7ee3a4fce02a589568a76d525b50452433
Britainy Richardson <Britainy Richardson>
2016-11-09 17:49:46 -0500

    updated data

---
+++ SQL/004-added donor records.sql
@@ -0,0 +1,14 @@
+insert into DONOR
+values ('John', 'Smith', '22 Walden Road', 'Jacksonville', 'FL', '32244', 'Duval', '01201962', 'Male', '9042221994', '', '9042425638', 'jsmith@aol.com', 'Gate Petroleum', 'Manager', '1101 Main Street', 'Jacksonville', 'FL', '32210', '3200', '', '');
+
+insert into DONOR
+values ('Darren', 'Hightower', '16 Orange Circle', 'Orange Park', 'FL', '32073', 'Clay', '08191989', 'Male', '3515627908', '', '', 'ClayLife123@yahoo.com.com', '', '', '', '', '', '', '100', '', '');
+
+insert into DONOR
+values ('Susan', 'Aurora', '1102 Cedar Point', 'Jacksonville', 'FL', '32256', 'Duval', '05161990', 'Female', '2985346924', '', '', 'SusieQ@gmail.com', '', '', '', '', '', '', '', 'Horse Trough', '');
+
+insert into DONOR
+values ('Georgine', 'Lee', '1806 Indian Hills Road', 'Jacksonville', 'FL', '32221', 'Duval', '09191988', 'Female', '8952671382', '', '9042786652', 'Surfer4Life@aol.com', 'Walmart', 'Cashier', '5191 CR 220', 'Fleming Island', 'FL', '32003', '', '', 'Mowed Lawn');
+
+insert into DONOR
+values ('Jacob', 'Brown', '112 Park Avenue', 'Orange Park', 'FL', '32073', 'Clay', '01201962', 'Male', '1112549835', '', '', 'jsmith@aol.com', '', '', '', '', '', '', '', 'Helmets', '');
