0ab6ecfbf28c5751f939bbc8bb19fcbb8da484eb
Duncan Hayward <Duncan Hayward>
2016-11-09 11:53:28 -0500

    Volunteer database (still doesn't add to database)

--- HopeTherapy/Controllers/VolunteerController.cs
+++ HopeTherapy/Controllers/VolunteerController.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using HopeTherapy.DataAccess;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace HopeTherapy.Controllers
 {
@@ -21,7 +25,25 @@ namespace HopeTherapy.Controllers
         {
             // Todo: Check database
             return View(model);
+        
+                    using (var cn = new SqlConnection(ConfigurationManager.ConnectionStrings["HopeTherapyIMS"].ConnectionString))
+            {
+                string sql = "INSERT INTO [dbo].[Volunteer] (VolunteerID, FirstName, LastName, Salutation, StreetAddress, City, States, ZipCode, County, CellPhoneNumber, HomePhoneNumber, OfficePhoneNumber, Email, Birthday, Gender, Job, DateOrientation, DateStarted, DaysVolunteered, HoursPerMonth)  VALUES(@VolunteerID, @FirstName, @LastName, @Salutation, @StreetAddress, @City, @States, @ZipCode, @County, @CellPhoneNumber, @HomePhoneNumber, @OfficePhoneNumber, @Email, @Birthday, @Gender, @Job, @DateOrientation, @DateStarted, @DaysVolunteered, @HoursPerMonth)";
+                try
+                {
+                    Utilities.Sql.ExecuteCommand(sql, model);
+                }
+                catch (SqlException ex)
+                {
+                    if(ex.Number == 2627) // 2627 = unique constraint violation.
+                    {
+                        ModelState.AddModelError("VolunteerID", "Volunteer is already in the system.");
+                        return View(model);
+                    }
+                    throw;
+                }
+              var volunteer = new Volunteer();
+            }
         }
-
     }
 }
