f878d9138e3f0c4e2a8e7fe1034fb082f241f9c2
Britainy Richardson <Britainy Richardson>
2016-11-10 13:21:35 -0500

    Donor & Volunteer Search Pages

---
+++ HopeTherapy/Controllers/DonorSearch.cs
@@ -0,0 +1,24 @@
+﻿using HopeTherapy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using HopeTherapy.DataAccess;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace HopeTherapy.Controllers
+{
+    public class DonorSearchController : Controller
+    {
+        // GET: Register
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+    }
+}
