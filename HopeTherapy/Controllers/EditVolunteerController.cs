8d7d1e17dc467454e51414ab5493447b4d4c146f
Duncan Hayward <Duncan Hayward>
2016-11-10 13:10:57 -0500

    edit Volunteer

--- HopeTherapy/Controllers/EditVolunteerController.cs
+++ HopeTherapy/Controllers/EditVolunteerController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using HopeTherapy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using HopeTherapy.DataAccess;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 namespace HopeTherapy.Controllers
 {
@@ -13,5 +19,31 @@ namespace HopeTherapy.Controllers
         {
             return View();
         }
+             public ActionResult Index(Volunteer model)
+        { 
+            using (var cn2 = new SqlConnection(ConfigurationManager.ConnectionStrings["HopeTherapyIMS"].ConnectionString))
+
+            {
+                string sql = "INSERT INTO [dbo].[Volunteer] (FirstName, LastName, Salutation, StreetAddress, City, States, ZipCode, County, CellPhoneNumber, HomePhoneNumber, WorkPhoneNumber, Email, Birthday, Gender, Job, DateOrientation, DateStarted, DaysVolunteered, HoursPerMonth)  VALUES(@FirstName, @LastName, @Salutation, @StreetAddress, @City, @States, @ZipCode, @County, @CellPhoneNumber, @HomePhoneNumber, @WorkPhoneNumber, @Email, @Birthday, @Gender, @Job, @DateOrientation, @DateStarted, @DaysVolunteered, @HoursPerMonth)";
+                try
+                {
+                    Utilities.Sql.ExecuteCommand(sql, model);
+                }
+                catch (SqlException)
+                {
+                    /*if(ex.Number == 2627) // 2627 = unique constraint violation.
+                    {
+                        ModelState.AddModelError("VolunteerID", "Volunteer is already in the system.");
+                        return View(model);
+                    }*/
+                    throw;
+                }
+
+            }
+            var Volunteer = new Volunteer();
+
+            return RedirectToAction("Index", "Home");
+        }
     }
+    
 }
