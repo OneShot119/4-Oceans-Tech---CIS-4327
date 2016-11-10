d66de955dba0aa535074b9eb975edfbca4929c6f
Britainy Richardson <Britainy Richardson>
2016-11-10 08:22:46 -0500

    Donation Info

--- HopeTherapy/Controllers/Donor.cs
+++ HopeTherapy/Controllers/Donor.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using HopeTherapy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using HopeTherapy.Models;
 using System.Web.Mvc;
+using System.Web.Security;
+using HopeTherapy.DataAccess;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace HopeTherapy.Controllers
 {
--- HopeTherapy/Models/Donor.cs
+++ HopeTherapy/Models/Donor.cs
@@ -18,22 +18,28 @@ namespace HopeTherapy.Models
         [Required(ErrorMessage = "Last Name is required"), DisplayName("Last Name")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Street Address is required"), DisplayName("Street Address")]
+//        [Required(ErrorMessage = "Street Address is required"), 
+        [DisplayName("Street Address")]
         public string StreetAddress { get; set; }
 
-        [Required(ErrorMessage = "City is required"), DisplayName("City Name")]
+//        [Required(ErrorMessage = "City is required"), 
+        [DisplayName("City Name")]
         public string City { get; set; }
 
-        [Required(ErrorMessage = "State is required"), DisplayName("State")]
+//        [Required(ErrorMessage = "State is required"), 
+        [DisplayName("State")]
         public string State { get; set; }
 
-        [Required(ErrorMessage = "Zip Code is required"), DisplayName("Zip Code")]
+//        [Required(ErrorMessage = "Zip Code is required"), 
+        [DisplayName("Zip Code")]
         public int ZipCode { get; set; }
 
-        [Required(ErrorMessage = "County is required"), DisplayName("County")]
+//        [Required(ErrorMessage = "County is required"), 
+        [DisplayName("County")]
         public string County { get; set; }
 
-        [Required(ErrorMessage = "Date of Birth is required"), DisplayName("Date of Birth")]
+//        [Required(ErrorMessage = "Date of Birth is required"), 
+        [DisplayName("Date of Birth")]
         public string Birthday { get; set; }
 
         public string Gender { get; set; }
@@ -48,7 +54,8 @@ namespace HopeTherapy.Models
         public int OfficePhoneNumber { get; set; }
 
         [DataType(DataType.EmailAddress)]
-        [Required(ErrorMessage = "Email is required"), DisplayName("Email")]
+//        [Required(ErrorMessage = "Email is required"), 
+        [DisplayName("Email")]
         public string EmailAddress { get; set; }
 
         public string CompanyName { get; set; }
