3cca8e69c5162c9e6cafd45ab2d03c78dd6426eb
David <dlang119@gmail.com>
2017-02-12 16:29:02 -0500

    daves stuff

--- HopeTherapy/Controllers/CalendarController.cs
+++ HopeTherapy/Controllers/CalendarController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HopeTherapy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,11 @@ namespace HopeTherapy.Controllers
     public class CalendarController : Controller
     {
         // GET: Calendar
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var days = Utilities.Sql.ExecuteQuery<Day>("SELECT Day as day, FirstName as fname, LastName as lname FROM Days JOIN Volunteer ON Days.Volunteer = Volunteer.VolunteerID");
+            return View(days);
         }
     }
 }
