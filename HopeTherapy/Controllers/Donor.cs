b6dbcaac4d25ce8c37c2e787c1942dde0c9a6cbf
David <dlang119@gmail.com>
2017-02-14 01:24:52 -0500

    edit donor

--- HopeTherapy/Controllers/Donor.cs
+++ HopeTherapy/Controllers/Donor.cs
@@ -20,6 +20,44 @@ namespace HopeTherapy.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var Donor = Utilities.Sql.ExecuteQuerySingleResult<Donor>("select D_CODE as DonorID, D_Fname as FirstName, D_Lname as LastName, D_address as StreetAddress, D_CITY as City, D_STATE as State, D_ZIP as ZipCode, D_County as County, D_BIRTHDAY as Birthday, D_GENDER as Gender, D_CELL_PHONE as CellPhoneNumber, D_HOME_PHONE as HomePhoneNumber, D_WORK_PHONE as WorkPhoneNumber, D_EMAIL as EmailAddress, D_CO_NAME as CompanyName, D_POSITION as Position, D_CO_ADDRESS as CompanyAddress, D_CO_STATE as CompanyState, D_CO_CITY as CompanyCity, D_CO_ZIP as CompanyZip, DONATION_DATE as DonationDate, DONATION_CURRENCY as CurrencyDonation, DONATION_ITEM as ItemDonation, DONATION_SERVICE as ServiceDonation from dbo.DONOR Where D_CODE = " +ID + ";");
+            return View(Donor);
+        }
+
+        [HttpPost]
+        public ActionResult EditDonor(Donor model)
+        {
+            // Todo: Check database
+
+
+
+            using (var cn2 = new SqlConnection(ConfigurationManager.ConnectionStrings["HopeTherapyIMS"].ConnectionString))
+
+            {
+                string sql = "UPDATE [dbo].[DONOR] SET D_Fname = @FirstName, D_Lname = @LastName, " +
+                    "D_address = @StreetAddress, D_CITY = @City, D_STATE = @State, D_ZIP = @ZipCode, D_County = @County, D_BIRTHDAY = @Birthday, " +
+                    "D_GENDER = @Gender, D_CELL_PHONE = @CellPhoneNumber, D_HOME_PHONE = @HomePhoneNumber, D_WORK_PHONE = @WorkPhoneNumber, " +
+                    "D_EMAIL = @EmailAddress, D_CO_NAME = @CompanyName, D_POSITION = @Position, D_CO_ADDRESS = @CompanyAddress, " +
+                    "D_CO_STATE = @CompanyState, D_CO_CITY = @CompanyCity, D_CO_ZIP = @CompanyZip, DONATION_DATE = @DonationDate, " +
+                    "DONATION_CURRENCY = @CurrencyDonation, DONATION_ITEM = @ItemDonation, DONATION_SERVICE = @ServiceDonation " +
+                    "WHERE D_CODE = @DonorID;";
+                try
+                {
+                    Utilities.Sql.ExecuteCommand(sql, model);
+                }
+                catch (SqlException ex)
+                {
+                    throw;
+                }
+
+            }
+
+            return RedirectToAction("List", "Donor");
+        }
+
         [HttpPost]
 
         public ActionResult Index(Donor model)
@@ -53,7 +91,7 @@ namespace HopeTherapy.Controllers
         [HttpGet]
         public ActionResult List()
         {
-            var emails = Utilities.Sql.ExecuteQuery<Donor>("select D_Fname as FirstName, D_Lname as LastName, D_EMAIL as EmailAddress, DONATION_DATE as DonationDate, DONATION_CURRENCY as CurrencyDonation, DONATION_ITEM as ItemDonation, DONATION_SERVICE as ServiceDonation from dbo.DONOR;");
+            var emails = Utilities.Sql.ExecuteQuery<Donor>("select D_CODE as DonorID, D_Fname as FirstName, D_Lname as LastName, D_EMAIL as EmailAddress, DONATION_DATE as DonationDate, DONATION_CURRENCY as CurrencyDonation, DONATION_ITEM as ItemDonation, DONATION_SERVICE as ServiceDonation from dbo.DONOR;");
             return View(emails);
 
         }
