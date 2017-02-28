1c49a15c4178ef4d645869641b83cc405327e1f9
bcrich0817 <bcrich0817@gmail.com>
2017-02-27 19:17:54 -0500

    states

--- HopeTherapy/Models/Donor.cs
+++ HopeTherapy/Models/Donor.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace HopeTherapy.Models
 {
@@ -26,6 +27,7 @@ namespace HopeTherapy.Models
 
         [DisplayName("State")]
         public string State { get; set; }
+        public IEnumerable<SelectListItem> StatesList { get; set; }
 
         [DisplayName("Zip Code")]
         public string ZipCode { get; set; }
