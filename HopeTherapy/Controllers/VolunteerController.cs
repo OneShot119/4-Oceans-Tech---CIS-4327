d0b138f163b073754ce39f5a3df7048b74b09480
OneShot119 <dlang119@gmail.com>
2017-04-10 14:57:05 -0400

    Fixes maybe?

--- HopeTherapy/Controllers/VolunteerController.cs
+++ HopeTherapy/Controllers/VolunteerController.cs
@@ -151,10 +151,10 @@ namespace HopeTherapy.Controllers
             {
                     List<String> Schedule = model.Schedule();
                     string sql = "INSERT INTO [dbo].[Volunteer] (FirstName, LastName, StreetAddress, City, States, ZipCode, County," +
-                        "CellPhoneNumber, HomePhoneNumber, WorkPhoneNumber, Email, Birthday, Gender, CompanyName, Position, CompanyAddress, CompanyZip, " +
+                        "CellPhoneNumber, HomePhoneNumber, WorkPhoneNumber, Email, Birthday, Gender, CompanyName, Position, CompanyAddress, " +
                         "DateOrientation, DateStarted, HoursPerMonth, VolunteerPosition, AreaOfInterest, Skills, Donor, Board, EmailList, MailList)" +
                         "VALUES(@FirstName, @LastName, @StreetAddress, @City, @States, @ZipCode, @County, @CellPhoneNumber, @HomePhoneNumber," +
-                        "@WorkPhoneNumber, @Email, @Birthday, @Gender, @CompanyName, @Position, @CompanyAddress, @CompanyZip," +
+                        "@WorkPhoneNumber, @Email, @Birthday, @Gender, @CompanyName, @Position, @CompanyAddress," +
                         "@DateOrientation, @DateStarted,  @HoursPerMonth, @VolunteerPosition, @AreaOfInterest, @Skills, @Donor, @Board, @EmailList, @MailList)";
                     try
                     {
